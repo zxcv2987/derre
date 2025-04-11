55a36e5770eb99f9ad3b9613ac71b989558e754b
태양 <zxcv2987@naver.com>
2025-04-12 02:11:59 +0900

    chore: svgr 패키지 추가 및 next.config 수정 - svgr 설정, 추후 이미지 업로드에 사용할 remotePattern 미리 설정

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,29 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: false,
+  logging: {
+    fetches: {
+      fullUrl: true,
+      hmrRefreshes: true,
+    },
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: process.env.NEXT_PUBLIC_STORAGE_HOSTNAME || "localhost",
+        search: "",
+      },
+    ],
+  },
+  turbopack: {
+    rules: {
+      "*.svg": {
+        loaders: ["@svgr/webpack"],
+        as: "*.js",
+      },
+    },
+  },
 };
-
 export default nextConfig;
