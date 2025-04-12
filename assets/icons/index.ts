80fa4848c44ab89597da768f587a48a2182670d2
태양 <zxcv2987@naver.com>
2025-04-13 00:32:32 +0900

    assets: icons index.ts 추가

---
+++ assets/icons/index.ts
@@ -0,0 +1,10 @@
+export { default as BellIcon } from "./icon_bell.svg";
+export { default as CreateIcon } from "./icon_create.svg";
+export { default as MoreIcon } from "./icon_more.svg";
+export { default as RankIcon } from "./icon_rank.svg";
+export { default as TipIcon } from "./icon_tip.svg";
+
+export { default as RankPageIcon } from "./page_rank.svg";
+export { default as HomePageIcon } from "./page_home.svg";
+export { default as ChatPageIcon } from "./page_chat.svg";
+export { default as UserPageIcon } from "./page_user.svg";
