b9ab8629cb1a5f78477a03ff7cd33e14ee496483
태양 <zxcv2987@naver.com>
2025-04-15 00:39:22 +0900

    feat: blog Post 삭제, 수정 api 추가

--- apis/blog.ts
+++ apis/blog.ts
@@ -1,6 +1,10 @@
 import { BlogPayload } from "@/types/payload";
 import { fetchClient } from "./fetchClient";
-import { BlogListResponseType, BlogResponseType } from "@/types/response";
+import {
+  BlogCreateResponseType,
+  BlogListResponseType,
+  BlogResponseType,
+} from "@/types/response";
 
 export async function createPost(
   payload: BlogPayload
@@ -38,6 +42,39 @@ export async function getBlogDetail(id: string): Promise<BlogResponseType> {
       "Content-Type": "application/json",
     },
     cache: "force-cache",
+  });
+
+  if (!res.ok) throw new Error(await res.text());
+  return res.json();
+}
+
+export async function deletePost(id: string): Promise<void> {
+  const res = await fetchClient(`/blog/${id}`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  if (!res.ok) throw new Error(await res.text());
+}
+
+export async function updatePost(
+  id: string,
+  payload: {
+    title: string;
+    content: string;
+    main_image?: string;
+    sub_image?: string;
+    category: number;
+  }
+): Promise<BlogCreateResponseType> {
+  const res = await fetchClient(`/blog/${id}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(payload),
   });
 
   if (!res.ok) throw new Error(await res.text());
