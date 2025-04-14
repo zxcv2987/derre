341488e2554a9edc9aa70f4428eb147a8c476e9d
태양 <zxcv2987@naver.com>
2025-04-14 23:20:33 +0900

    fix: components/domain 경로 변경

--- app/blog/create/page.tsx
+++ app/blog/create/page.tsx
@@ -1,6 +1,6 @@
 import { getCategory } from "@/apis/category";
 import PreventLeaveWrapper from "@/components/common/preventLeaveWrapper";
-import CreatePostForm from "@/components/domain/blog/createPostForm";
+import CreatePostForm from "@/components/domain/blog/create/createPostForm";
 
 export default async function Page() {
   const categories = (await getCategory()).data;
