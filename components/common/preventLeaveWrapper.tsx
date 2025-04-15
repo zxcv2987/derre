f1d6f70d8a485d11128d2c2770b31f8f25af0091
태양 <zxcv2987@naver.com>
2025-04-15 20:41:45 +0900

    fix: prventLaeaveWrapper useMemo 삭제

--- components/common/preventLeaveWrapper.tsx
+++ components/common/preventLeaveWrapper.tsx
@@ -1,38 +1,29 @@
 "use client";
 
 import usePreventLeave from "@/utils/hooks/preventLeave/usePreventLeave";
-import { useMemo } from "react";
-import Modal from "./modal/composition/ModalRoot";
-
+import { useRouter } from "next/navigation";
+import Modal from "@/components/common/modal/composition/ModalRoot";
 export default function PreventLeaveWrapper({
   children,
 }: {
   children: React.ReactNode;
 }) {
   const { openModal, handlePopStateModalClose } = usePreventLeave();
-
-  const LeaveModals = useMemo(
-    () => (
+  const router = useRouter();
+  return (
+    <>
       <Modal.Root onClose={handlePopStateModalClose} isOpen={openModal}>
         <Modal.Title>작성중인 내용이 삭제됩니다.</Modal.Title>
         <Modal.ActionsContainer>
-          <Modal.Action onClick={handlePopStateModalClose}>
+          <Modal.Action onClick={handlePopStateModalClose} variant="primary">
             계속하기
           </Modal.Action>
-          <Modal.Action onClick={() => window.history.back()}>
+          <Modal.Action onClick={() => router.back()} variant="danger">
             나가기
           </Modal.Action>
         </Modal.ActionsContainer>
       </Modal.Root>
-    ),
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    [openModal]
-  );
-
-  return (
-    <>
       {children}
-      {LeaveModals}
     </>
   );
 }
